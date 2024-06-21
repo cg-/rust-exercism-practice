80ad1d12b9b3101f369ab919225b690b370e890b
Cole Grim <cg@netfla.me>
2024-06-20 19:53:54 -0700

    finished grains

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,11 +1,11 @@
 pub fn square(s: u32) -> u64 {
     match s{
-        0 => panic!("Square must be between 1 and 64")
-        >64 => panic!("Square must be between 1 and 64")
-        _ => 2_u64.pow(s-1)
+        0 => panic!("Square must be between 1 and 64"),
+        65..=u32::MAX => panic!("Square must be between 1 and 64"),
+        _ => 2_u64.pow(s-1),
     }
 }
 
 pub fn total() -> u64 {
-    (1)
+    (1..=64).fold(0, |acc, n| acc + square(n))
 }
