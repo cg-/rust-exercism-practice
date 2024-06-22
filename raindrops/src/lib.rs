9829cbe0bd8ea6427ddd6320a71b83c9bbe13130
Cole Grim <cg@netfla.me>
2024-06-22 09:23:40 -0700

    raindrops

---
+++ raindrops/src/lib.rs
@@ -0,0 +1,27 @@
+/*
+- is divisible by 3, add "Pling" to the result.
+- is divisible by 5, add "Plang" to the result.
+- is divisible by 7, add "Plong" to the result.
+- **is not** divisible by 3, 5, or 7, the result should be the number as a string.
+*/
+
+pub fn raindrops(n: u32) -> String {
+    let mut to_ret = String::new();
+    let mut found = false;
+    if n % 3{
+        found = true;
+        to_ret = format!("{}{}",to_ret, "Pling");
+    }
+    if n % 5{
+        found = true;
+        to_ret = format!("{}{}",to_ret, "Plang");
+    }
+    if n % 7{
+        found = true;
+        to_ret = format!("{}{}",to_ret, "Plong");
+    }
+    if !found {
+        to_ret = format!("{}",n);
+    }
+    to_ret
+}
